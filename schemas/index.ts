87523dfe5bf6f23ca5059d22baa0750fb7cd2197
Francisco cruz <megafran@msn.com>
2023-10-21 09:41:26 -0500

    feature: removed not needed documents, just kept email

--- schemas/index.ts
+++ schemas/index.ts
@@ -1,14 +1,8 @@
 import { type SchemaTypeDefinition } from "sanity"
-import post from "./post"
-import author from "./author"
-import blockContent from "./blockContent"
-import category from "./category"
-import company from "./company"
 import email from "./email"
-import home from "./pages/home"
 
 const schema: { types: SchemaTypeDefinition[] } = {
-  types: [post, author, blockContent, category, company, home, email],
+  types: [email],
 }
 
 export default schema
